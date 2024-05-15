58dcd6b8e14be0f38caea01d20b368b950485772
Mohtasim Alam <94981761+codeblitz97@users.noreply.github.com>
2024-05-15 17:30:59 +0600

    Create video player

---
+++ src/components/shared/Video.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function RenderVideo({
+  trailer,
+}: Readonly<{ trailer: string }>) {
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const handleVideoEnded = () => {
+    setIsPlaying(true);
+  };
+
+  return (
+    <div className="flex items-center justify-center overflow-hidden">
+      <video
+        src={trailer as string}
+        preload="auto"
+        autoPlay={isPlaying}
+        muted
+        onEnded={handleVideoEnded}
+        className="aspect-video h-[300px] min-w-[500px] rounded-lg object-cover md:h-[350px] md:min-w-[1000px] lg:h-[600px] lg:min-w-[2000px]"
+      ></video>
+    </div>
+  );
+}
