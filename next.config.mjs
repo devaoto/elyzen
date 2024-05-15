a3810a330650b82cf481c9ffcb6b96f3748df813
Mohtasim Alam <94981761+codeblitz97@users.noreply.github.com>
2024-05-15 17:28:55 +0600

    Install necessary module and use shadcn

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,45 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 's4.anilist.co',
+        port: '',
+        pathname: '/**',
+        protocol: 'https',
+      },
+      {
+        hostname: 'artworks.thetvdb.com',
+        port: '',
+        pathname: '/**',
+        protocol: 'https',
+      },
+      {
+        hostname: 'media.kitsu.io',
+        port: '',
+        pathname: '/**',
+        protocol: 'https',
+      },
+      {
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/**',
+        protocol: 'https',
+      },
+      {
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+        protocol: 'https',
+      },
+      {
+        hostname: 'img1.ak.crunchyroll.com',
+        port: '',
+        pathname: '/**',
+        protocol: 'https',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
