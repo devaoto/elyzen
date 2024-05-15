deebc09d52332a89d98257604181037bd7d0b754
Mohtasim Alam <94981761+codeblitz97@users.noreply.github.com>
2024-05-15 17:29:51 +0600

    Declare utility function

---
+++ src/lib/utils.ts
@@ -0,0 +1,46 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function getRandomElement<T>(arr: T[]): T | undefined {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+
+export function numberToMonth(num: number): string {
+  // Use a switch statement to match the input number to the corresponding month
+  switch (num) {
+    case 1:
+      return 'January';
+    case 2:
+      return 'February';
+    case 3:
+      return 'March';
+    case 4:
+      return 'April';
+    case 5:
+      return 'May';
+    case 6:
+      return 'June';
+    case 7:
+      return 'July';
+    case 8:
+      return 'August';
+    case 9:
+      return 'September';
+    case 10:
+      return 'October';
+    case 11:
+      return 'November';
+    case 12:
+      return 'December';
+    default:
+      return 'January';
+  }
+}
