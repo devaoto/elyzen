5f48ced66d9a73b1330944b8f3113d4cbf1a0da2
Mohtasim Alam <94981761+codeblitz97@users.noreply.github.com>
2024-05-16 21:32:35 +0600

    Add drawer and sperator

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -24,7 +24,7 @@ export function ModeToggle() {
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent className="sticky z-[69423535]" align="end">
         <DropdownMenuItem onClick={() => setTheme('light')}>
           Light
         </DropdownMenuItem>
