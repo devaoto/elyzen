bc88531548851595b3e6f0587455052f947c8c76
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-05-21 22:59:35 +0600

    Change the font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { getTrendingAnime, getPopularAnime } from '@/lib/anime';
 import { Slider } from '@/components/shared/Slider';
 import dynamic from 'next/dynamic';
 import Footer from '@/components/Footer';
+import { Flame, Star } from 'lucide-react';
 
 const SideBar = dynamic(() => import('@/components/SideBar'), { ssr: false });
 
@@ -19,15 +20,19 @@ export default function Home() {
       <div className='ml-0 md:ml-16 lg:ml-16 xl:ml-16 2xl:ml-16'>
         <Hero data={trending} />
         <div className='mt-20'>
-          <h1 className='mb-4 font-mono text-4xl'>TRENDING NOW</h1>
+          <h1 className='mb-4 flex gap-1 text-4xl font-bold'>
+            <Flame className='size-9' /> <span>Trending Now</span>
+          </h1>
           <Slider data={trending} title='trending' />
         </div>
         <div className='mt-20'>
-          <h1 className='mb-4 font-mono text-4xl'>ALL TIME POPULAR</h1>
+          <h1 className='mb-4 flex gap-1 text-4xl font-bold'>
+            <Star className='size-9' /> All Time Popular
+          </h1>
           <Slider data={popular} title='popular' />
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
