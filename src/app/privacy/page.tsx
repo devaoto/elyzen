80694ef024404b9f20d23f4b246f404e1bd33067
Sunil Gupta <sunilk98850@gmail.com>
2024-05-22 12:44:03 +0530

    fix(privacy): "changed website name from Reveraki to Elyzen"

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -10,12 +10,12 @@ const PrivacyPolicy = () => {
         <h1 className='mb-4 font-mono text-3xl'>PRIVACY POLICY</h1>
         <h1 className='mb-4 text-xl'>Last updated: 05/06/2024</h1>
         <p className='mb-4 text-lg'>
-          At Reveraki, we take your privacy seriously. We are committed to
+          At Elyzen, we take your privacy seriously. We are committed to
           protecting the privacy of our users and ensuring that all personal
           information provided to us remains confidential.
         </p>
         <p className='mb-4 text-lg'>
-          Reveraki does not collect any personal information from its users. We
+          Elyzen does not collect any personal information from its users. We
           do not use cookies or any other tracking technologies. We only store a
           few settings locally when the app is accessed from localhost. You can
           remove your local storage data by clearing your browser site cache or
@@ -23,12 +23,12 @@ const PrivacyPolicy = () => {
         </p>
         <p className='mb-4 text-lg'>
           Our app is designed to respect your privacy and anonymity. You can use
-          Reveraki with the assurance that your personal information will never
+          Elyzen with the assurance that your personal information will never
           be compromised.
         </p>
         <p className='mb-12 text-lg'>
           If you have any questions or concerns about our privacy policy, please
-          feel free to contact us at privacy@reveraki.com.
+          feel free to contact us at privacy@Elyzen.com.
         </p>
       </div>
       <Footer />
