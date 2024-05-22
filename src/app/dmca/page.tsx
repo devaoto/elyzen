f73bb192186b6c1b5965623579e713b9c3b86405
Sunil Gupta <sunilk98850@gmail.com>
2024-05-22 12:35:23 +0530

    fix(DMCA): "changed website name from Reveraki to Elyzen"

--- src/app/dmca/page.tsx
+++ src/app/dmca/page.tsx
@@ -9,12 +9,12 @@ const DMCA = () => {
       <div className='mx-auto max-w-2xl px-4 py-20'>
         <h1 className='mb-4 font-mono text-4xl'>DMCA NOTE</h1>
         <p className='mb-56 text-primary-200 text-lg'>
-          ReverAki operates independently and is not formally associated with
+          Elyzen operates independently and is not formally associated with
           nor endorsed by any of the anime studios responsible for the creation
           of the anime featured on this platform. Our website serves solely as a
           user interface, facilitating access to self-hosted files sourced from
           various third-party providers across the internet. It&apos;s important
-          to note that ReverAki never initiates downloads of video content from
+          to note that Elyzen never initiates downloads of video content from
           these providers. Instead, links are provided in response to user
           requests, thereby absolving the platform from any potential DMCA
           compliance issues.
