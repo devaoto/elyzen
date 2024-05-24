789b44992fb36cd2de769d6aba8124066161ec1e
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-05-24 11:49:23 +0600

    Update globals.d.ts

--- globals.d.ts
+++ globals.d.ts
@@ -7,3 +7,15 @@ declare namespace NodeJS {
     MALSYNC_URI: string;
   }
 }
+
+// If bun supports NextJs any soon. Uncomment this:
+
+// declare module 'bun' {
+//   interface Env {
+//     REDIS_URI?: string;
+//     NEXT_PUBLIC_DOMAIN: string;
+//     HIANIME_API: string;
+//     CONSUMET_API: string;
+//     MALSYNC_URI: string;
+//   }
+// }
