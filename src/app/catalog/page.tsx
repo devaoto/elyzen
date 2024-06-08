ed3f8506e1ab5340927b0e77d817d91f351cbabd
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-06-08 06:32:46 +0600

    Wrap the full code in a Suspense boundry

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -12,7 +12,7 @@ import {
   DropdownItem,
 } from '@nextui-org/react';
 import useDebounce from '@/hooks/useDebounce';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import { ReturnData } from '@/types/animeData';
 import { Card } from '@/components/shared/Card';
@@ -145,129 +145,133 @@ export default function Catalog() {
   ];
 
   return (
-    <div className='ml-5'>
-      <h1 className='mb-20 text-3xl font-bold'>Catalog</h1>
-      <div className='flex flex-col gap-4 lg:flex-row'>
-        <div>
-          <div className='mb-2 min-w-full max-w-full md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
-            <Input
-              label='Query'
-              value={query ?? undefined}
-              onChange={(e) => setQuery(e.target.value)}
-            />
-          </div>
-          <div className='mb-2 min-w-full max-w-full md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
-            <Input
-              color='primary'
-              variant='bordered'
-              label='Year (1998 - 2024)'
-              value={year ?? undefined}
-              onChange={(e) => setYear(e.target.value)}
-            />
-          </div>
-          <div className='min-w-full max-w-full rounded-lg bg-background/90 p-2 shadow-md md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
-            <RadioGroup
-              label='Select a season to search'
-              color='primary'
-              value={season ?? undefined}
-              onValueChange={(value) => setSeason(value)}
-            >
-              <Radio value='SPRING'>Spring</Radio>
-              <Radio value='SUMMER'>Summer</Radio>
-              <Radio value='FALL'>Fall</Radio>
-              <Radio value='WINTER'>Winter</Radio>
-            </RadioGroup>
-          </div>
-          <div className='mt-2 min-w-full max-w-full rounded-lg bg-background/90 p-2 shadow-md md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
-            <RadioGroup
-              label='Select a format to search'
-              color='primary'
-              value={format!}
-              onValueChange={(value) => setFormat(value)}
-            >
-              <Radio value='TV'>TV</Radio>
-              <Radio value='TV_SHORT'>TV Short</Radio>
-              <Radio value='OVA'>OVA</Radio>
-              <Radio value='ONA'>ONA</Radio>
-              <Radio value='MOVIE'>Movie</Radio>
-              <Radio value='SPECIAL'>Special</Radio>
-              <Radio value='MUSIC'>Music</Radio>
-            </RadioGroup>
-          </div>
-          <div className='mt-2 min-w-full max-w-full rounded-lg bg-background/90 p-2 shadow-md md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
-            <Dropdown>
-              <DropdownTrigger>
-                <Button variant='flat'>
-                  Sort by:{' '}
-                  {sort.length > 0
-                    ? sort
-                        .map(
-                          (s) =>
-                            sortOptions.find((option) => option.value === s)
-                              ?.label
-                        )
-                        .join(', ')
-                    : 'Select Sort'}
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                aria-label='Sort Options'
+    <Suspense>
+      <div className='ml-5'>
+        <h1 className='mb-20 text-3xl font-bold'>Catalog</h1>
+        <div className='flex flex-col gap-4 lg:flex-row'>
+          <div>
+            <div className='mb-2 min-w-full max-w-full md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
+              <Input
+                label='Query'
+                value={query ?? undefined}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </div>
+            <div className='mb-2 min-w-full max-w-full md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
+              <Input
                 color='primary'
-                selectionMode='multiple'
-                selectedKeys={sort} /* @ts-ignore */
-                onSelectionChange={(keys) => setSort([...keys])}
+                variant='bordered'
+                label='Year (1998 - 2024)'
+                value={year ?? undefined}
+                onChange={(e) => setYear(e.target.value)}
+              />
+            </div>
+            <div className='min-w-full max-w-full rounded-lg bg-background/90 p-2 shadow-md md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
+              <RadioGroup
+                label='Select a season to search'
+                color='primary'
+                value={season ?? undefined}
+                onValueChange={(value) => setSeason(value)}
               >
-                {sortOptions.map((option) => (
-                  <DropdownItem key={option.value}>{option.label}</DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
-          </div>
-          <div className='mt-4 min-w-full max-w-full md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
-            <h2 className='mb-2 text-lg font-semibold'>Genres</h2>
-            <div className='flex flex-wrap gap-2'>
-              {[
-                'Action',
-                'Adventure',
-                'Cars',
-                'Comedy',
-                'Drama',
-                'Fantasy',
-                'Horror',
-                'Mahou Shoujo',
-                'Mecha',
-                'Music',
-                'Mystery',
-                'Psychological',
-                'Romance',
-                'Sci-Fi',
-                'Slice of Life',
-                'Sports',
-                'Supernatural',
-                'Thriller',
-              ].map((genre) => (
-                <Button
-                  key={genre}
-                  onPress={() => toggleGenre(genre)}
-                  color={isGenreSelected(genre) ? 'primary' : 'default'}
-                  variant={isGenreSelected(genre) ? 'solid' : 'ghost'}
-                  className='capitalize'
+                <Radio value='SPRING'>Spring</Radio>
+                <Radio value='SUMMER'>Summer</Radio>
+                <Radio value='FALL'>Fall</Radio>
+                <Radio value='WINTER'>Winter</Radio>
+              </RadioGroup>
+            </div>
+            <div className='mt-2 min-w-full max-w-full rounded-lg bg-background/90 p-2 shadow-md md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
+              <RadioGroup
+                label='Select a format to search'
+                color='primary'
+                value={format!}
+                onValueChange={(value) => setFormat(value)}
+              >
+                <Radio value='TV'>TV</Radio>
+                <Radio value='TV_SHORT'>TV Short</Radio>
+                <Radio value='OVA'>OVA</Radio>
+                <Radio value='ONA'>ONA</Radio>
+                <Radio value='MOVIE'>Movie</Radio>
+                <Radio value='SPECIAL'>Special</Radio>
+                <Radio value='MUSIC'>Music</Radio>
+              </RadioGroup>
+            </div>
+            <div className='mt-2 min-w-full max-w-full rounded-lg bg-background/90 p-2 shadow-md md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
+              <Dropdown>
+                <DropdownTrigger>
+                  <Button variant='flat'>
+                    Sort by:{' '}
+                    {sort.length > 0
+                      ? sort
+                          .map(
+                            (s) =>
+                              sortOptions.find((option) => option.value === s)
+                                ?.label
+                          )
+                          .join(', ')
+                      : 'Select Sort'}
+                  </Button>
+                </DropdownTrigger>
+                <DropdownMenu
+                  aria-label='Sort Options'
+                  color='primary'
+                  selectionMode='multiple'
+                  selectedKeys={sort} /* @ts-ignore */
+                  onSelectionChange={(keys) => setSort([...keys])}
                 >
-                  {genre}
-                </Button>
+                  {sortOptions.map((option) => (
+                    <DropdownItem key={option.value}>
+                      {option.label}
+                    </DropdownItem>
+                  ))}
+                </DropdownMenu>
+              </Dropdown>
+            </div>
+            <div className='mt-4 min-w-full max-w-full md:min-w-full md:max-w-full lg:min-w-[300px] lg:max-w-[300px]'>
+              <h2 className='mb-2 text-lg font-semibold'>Genres</h2>
+              <div className='flex flex-wrap gap-2'>
+                {[
+                  'Action',
+                  'Adventure',
+                  'Cars',
+                  'Comedy',
+                  'Drama',
+                  'Fantasy',
+                  'Horror',
+                  'Mahou Shoujo',
+                  'Mecha',
+                  'Music',
+                  'Mystery',
+                  'Psychological',
+                  'Romance',
+                  'Sci-Fi',
+                  'Slice of Life',
+                  'Sports',
+                  'Supernatural',
+                  'Thriller',
+                ].map((genre) => (
+                  <Button
+                    key={genre}
+                    onPress={() => toggleGenre(genre)}
+                    color={isGenreSelected(genre) ? 'primary' : 'default'}
+                    variant={isGenreSelected(genre) ? 'solid' : 'ghost'}
+                    className='capitalize'
+                  >
+                    {genre}
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className='flex-grow'>
+            <h2 className='mb-4 text-2xl font-semibold'>Results</h2>
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+              {result?.results.map((anime) => (
+                <Card key={anime.id} anime={anime} />
               ))}
             </div>
           </div>
         </div>
-        <div className='flex-grow'>
-          <h2 className='mb-4 text-2xl font-semibold'>Results</h2>
-          <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
-            {result?.results.map((anime) => (
-              <Card key={anime.id} anime={anime} />
-            ))}
-          </div>
-        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
