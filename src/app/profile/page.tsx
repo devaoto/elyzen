4ba7b01e82aae332fbc6243bb30db72683879993
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-06-19 00:47:24 +0600

    Add better user information

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation';
 import { Image } from '@nextui-org/react';
 import { getRelativeTime } from '@/lib/utils';
 import dynamic from 'next/dynamic';
+import { getAnilistUser } from '@/lib/authenticated';
 
 const SideBar = dynamic(() => import('@/components/SideBar'), { ssr: false });
 
@@ -12,15 +13,13 @@ export default function Profile() {
 
   if (!profile || !profile.user) redirect('/');
 
-  console.log(profile.user);
+  console.log(profile);
+  const actualProfile = use(
+    getAnilistUser((profile.user as any).token, profile.user.name!)
+  );
 
-  const { name, image, createdAt, bannerImage } = profile.user as unknown as {
-    name: string;
-    image: { large: string; medium: string };
-    bannerImage?: string;
-    createdAt: number;
-  };
-  const profilePicture = image?.large || image?.medium || null;
+  const user = actualProfile?.data?.User;
+  const profilePicture = user?.avatar?.large || user?.avatar?.medium || null;
 
   return (
     <>
@@ -28,33 +27,33 @@ export default function Profile() {
         <SideBar />
       </div>
 
-      <div className='min-h-screen bg-background text-foreground'>
-        {bannerImage ? (
+      <div className='min-h-screen overflow-x-hidden bg-background text-foreground'>
+        {user?.bannerImage ? (
           <Image
             radius='none'
-            className='h-56 w-full object-cover sm:h-72 md:h-96'
-            src={bannerImage}
+            className='h-56 min-w-[1920px] object-cover sm:h-72 md:h-96'
+            src={user?.bannerImage}
             draggable={false}
-            alt={`Banner image ${name}`}
+            alt={`Banner image ${user?.name}`}
           />
         ) : (
           <>
             <div className='h-56 w-full bg-[#00FF7F] object-cover sm:h-72 md:h-96' />
           </>
         )}
-        <div className='flex flex-col items-center p-6'>
+        <div className='relative z-10 flex flex-col items-center p-6'>
           {profilePicture && (
             <img
               src={profilePicture}
-              alt={`Profile picture of ${name}`}
+              alt={`Profile picture of ${user?.name}`}
               className='-mt-16 h-32 w-32 rounded-full border-4 border-white dark:border-gray-900 sm:h-40 sm:w-40 md:h-48 md:w-48'
             />
           )}
           <h1 className='mt-4 text-2xl font-semibold sm:text-3xl md:text-4xl'>
-            {name}
+            {user?.name}
           </h1>
           <p className='mt-2 text-sm text-gray-600 dark:text-gray-400 sm:text-base md:text-lg'>
-            Joined {getRelativeTime(createdAt)}
+            Joined {getRelativeTime(user?.createdAt!)}
           </p>
         </div>
       </div>
--- src/lib/authenticated.ts
+++ src/lib/authenticated.ts
@@ -9,11 +9,28 @@ interface SaveProgressResponse {
   };
 }
 
+export interface AnilistUserResponse {
+  User: {
+    about: string;
+    avatar: {
+      large: string;
+      medium: string;
+    };
+    bannerImage: string;
+    createdAt: number;
+    donatorBadge: string;
+    donatorTier: number;
+    id: number;
+    name: string;
+    updatedAt: number;
+  };
+}
+
 const executeGraphQlQuery = async <T = any>(
   authToken: string | null,
   gqlQuery: string,
   queryVariables: Record<string, any>
-): Promise<{ data?: T; errors?: any } | void> => {
+): Promise<{ data?: T; errors?: any } | null> => {
   try {
     const response = await fetch('https://graphql.anilist.co/', {
       method: 'POST',
@@ -28,6 +45,7 @@ const executeGraphQlQuery = async <T = any>(
     return response.json();
   } catch (error) {
     console.error('An error occurred, please try again later');
+    return null;
   }
 };
 
@@ -67,4 +85,28 @@ export const saveProgress = async (
     console.error('An error occurred while updating the list');
     toast.error('An error occurred while updating the list');
   }
+};
+
+export const getAnilistUser = async (
+  token: string,
+  name: string
+): Promise<{ data?: AnilistUserResponse; errors?: any } | null> => {
+  const query = `query($name: String)  {
+  User(name: $name) {
+    about
+    avatar {
+      large
+      medium
+    }
+    bannerImage
+    createdAt
+    donatorBadge
+    donatorTier
+    id
+    name
+    updatedAt
+  }
+}`;
+
+  return executeGraphQlQuery<AnilistUserResponse>(token, query, { name });
 };
