934036d28d1a148c3983b707a05adacce326abc4
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-06-18 18:09:58 +0600

    Add better metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,21 @@ import {
 } from '@/lib/anime';
 import { ReturnData } from '@/types/animeData';
 import dynamic from 'next/dynamic';
+import { Metadata } from 'next';
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const trending = (await getTrendingAnime(1, 5)) as ReturnData;
+
+  const trendingTitles = trending.results.map(
+    (result) =>
+      result.title.english ?? result.title.romaji ?? result.title.native
+  );
+
+  return {
+    title: `Elyzen - Watch your favorite anime in one place`,
+    description: `Watch ${trendingTitles.join(', ')} on Elyzen!`,
+  };
+};
 
 const HomeComp = dynamic(() => import('./home'), { ssr: false });
 
